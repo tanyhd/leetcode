589a78666fd8ee9d330609903e6d11405fe4e99c
DarrenT <darren243@hotmail.com>
2025-08-15 09:11:03 +0800

    342 golang

---
+++ Golang/Easy/342_Power_of_Four/solution.go
@@ -0,0 +1,13 @@
+package poweroffour
+
+func isPowerOfFour(n int) bool {
+	if n == 0 {
+		return false
+	}
+
+	for n%4 == 0 {
+		n /= 4
+	}
+
+	return n == 1
+}
---
+++ Golang/Easy/342_Power_of_Four/solution_test.go
@@ -0,0 +1,24 @@
+package poweroffour
+
+import "testing"
+
+func TestIsPowerOfThree(t *testing.T) {
+	tests := []struct {
+		name     string
+		n        int
+		expected bool
+	}{
+		{"Case 1", 16, true},
+		{"Case 2", 5, false},
+		{"Case 3", 1, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := isPowerOfFour(tt.n)
+			if result != tt.expected {
+				t.Errorf("expected %v, got %v", tt.expected, result)
+			}
+		})
+	}
+}
